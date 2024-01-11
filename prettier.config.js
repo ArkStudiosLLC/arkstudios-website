7d3df3099f0b335e56cf6d3944aa400523c27c0e
たつぞう <tatsuzoaraki@gmail.com>
2024-01-11 21:50:50 +0900

    Setup linter and formatter tools

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+/** @type {import('prettier').Config} */
+
+module.exports = {
+  plugins: ['prettier-plugin-tailwindcss'],
+  semi: false,
+  singleQuote: true,
+  printWidth: 90,
+  tabWidth: 2,
+  trailingComma: 'all',
+  jsxSingleQuote: true,
+}
