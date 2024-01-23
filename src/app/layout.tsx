b9d0ff5b0df326a20c96c3113db386cec3ef70b0
たつぞう <tatsuzoaraki@gmail.com>
2024-01-23 20:21:34 +0900

    Improve favicons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,6 +12,44 @@ export const metadata: Metadata = {
   },
   description: '選び抜かれたソリューション、未来への方舟。',
   metadataBase: new URL('https://arkstudios.co.jp'),
+  icons: {
+    icon: '/images/favicons/favicon.ico',
+    shortcut: '/images/favicons/favicon.ico',
+    apple: '/images/favicons/apple-touch-icon.png',
+    other: [
+      {
+        url: '/images/favicons/favicon-256x256.png',
+        type: 'image/png',
+        sizes: '256x256',
+        rel: 'icon',
+      },
+      {
+        url: '/images/favicons/favicon-32x32.png',
+        type: 'image/png',
+        sizes: '32x32',
+        rel: 'icon',
+      },
+      {
+        url: '/images/favicons/favicon-16x16.png',
+        type: 'image/png',
+        sizes: '16x16',
+        rel: 'icon',
+      },
+      {
+        url: '/images/favicons/safari-pinned-tab.svg',
+        rel: 'mask-icon',
+        color: '#000000',
+      },
+      {
+        url: '/site.webmanifest',
+        rel: 'manifest',
+      },
+    ],
+  },
+  other: {
+    'msapplication-TileColor': '#164e63',
+    'msapplication-config': '/browserconfig.xml',
+  },
 }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
