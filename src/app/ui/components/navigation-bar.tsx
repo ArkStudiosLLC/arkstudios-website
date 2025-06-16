8a5f49940aa9a30d102e0de784e422203114e31e
Tatsuzo <tatsuzoaraki@gmail.com>
2025-06-16 15:55:48 +0900

    Resolve DOM property issues

--- src/app/ui/components/navigation-bar.tsx
+++ src/app/ui/components/navigation-bar.tsx
@@ -64,10 +64,10 @@ function LanguageToggle({
         <svg
           stroke='currentColor'
           fill='none'
-          stroke-width='2'
+          strokeWidth='2'
           viewBox='0 0 24 24'
-          stroke-linecap='round'
-          stroke-linejoin='round'
+          strokeLinecap='round'
+          strokeLinejoin='round'
           aria-hidden='true'
           focusable='false'
           xmlns='http://www.w3.org/2000/svg'
